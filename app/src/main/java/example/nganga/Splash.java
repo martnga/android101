928fdff19c090519b0bc97fe4ff07636fbcc5245
mtnjoroge <mtnjoroge>
2015-07-24 12:25:52 +0300

    linked the splash.class to xml file

--- app/src/main/java/example/nganga/Splash.java
+++ app/src/main/java/example/nganga/Splash.java
@@ -1,7 +1,16 @@
 package example.nganga;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by nganga on 7/24/15.
  */
-public class Splash {
+public class Splash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+    }
 }
--- app/src/main/java/example/nganga/main.java
+++ app/src/main/java/example/nganga/main.java
@@ -1,12 +1,13 @@
 package example.nganga;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class main extends ActionBarActivity {
+public class main extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
