0a63ff2b8cfcbd0cb07d3229603450bda5970b92
mtnjoroge <mtnjoroge@gmail.com>
2015-07-25 20:23:24 +0300

    inflater view for the fragment layouts

--- app/src/main/java/example/nganga/FragmentTwo.java
+++ app/src/main/java/example/nganga/FragmentTwo.java
@@ -1,7 +1,20 @@
 package example.nganga;
 
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by nganga on 7/25/15.
  */
-public class FragmentTwo {
+public class FragmentTwo extends Fragment{
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_two_layout, container,false);
+        return v;
+    }
 }
