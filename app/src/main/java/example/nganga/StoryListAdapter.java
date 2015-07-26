51ffd4749e1fda0df3ad39f9f799fa3fc54ae5f2
mtnjoroge <mtnjoroge@gmail.com>
2015-07-26 12:27:14 +0300

    storylist adapter extends Recyclerview

--- app/src/main/java/example/nganga/StoryListAdapter.java
+++ app/src/main/java/example/nganga/StoryListAdapter.java
@@ -2,39 +2,30 @@ package example.nganga;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 
 /**
  * Created by nganga on 7/25/15.
  */
-public class StoryListAdapter extends Activity {
+public class StoryListAdapter extends RecyclerView.Adapter<StoryListAdapter.CardViewHolder> {
+
+    private int mNumCards;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.story_list);
+    public StoryListAdapter.CardViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(StoryListAdapter.CardViewHolder holder, int position) {
 
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public int getItemCount() {
+        return 0;
     }
 }
